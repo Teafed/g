82c774efb399127b2533055aad82a6e600f9933c
Teafed <xjakea45x@yahoo.com>
2025-07-30 21:29:17 -0700

    implemented cli flags

--- src/main.c
+++ src/main.c
@@ -19,21 +19,26 @@ typedef struct {
 
 Game g_game = { 0 }; // should this be static?
 
-bool game_init(void);
+bool game_init(float scale_factor, int framerate);
 void game_update(float delta_time);
 void game_render(void);
 void game_handle_events(float delta_time);
 void game_shutdown(void);
+bool game_handle_flags(int argc, char *argv[], int* logging_mode, float* scale_factor, int* framerate);
 
 int main(int argc, char* argv[]) {
-   if (argc > 1)
-      LOG_VERBOSITY = atoi(argv[1]);
-   
-   if (d_dne(game_init())) {
-      printf("failed to initialize game\n");
-      return 1;
+   {  // initialize w flags
+      int logging_mode = 1;
+      float scale_factor = 1.0f;
+      int framerate = 60;
+      if (!game_handle_flags(argc, argv, &logging_mode, &scale_factor, &framerate))
+         return 1;
+      if (!game_init(scale_factor, framerate)) {
+         d_err("failed to initialize game");
+         return 1;
+      }
    }
-
+   
    while (g_game.state == GAME_RUNNING) {
       timing_frame_start();
       
@@ -54,15 +59,15 @@ int main(int argc, char* argv[]) {
    return 0;
 }
 
-bool game_init(void) {
+bool game_init(float scale_factor, int framerate) {
    // init renderer
    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-      SDL_Log("SDL could not initialize! SDL_Error: %s", SDL_GetError());
+      d_err("SDL could not initialize! SDL_Error: %s", SDL_GetError());
       return false;
    }
 
-   timing_init(60);   
-   if (!renderer_init(1)) return false;
+   timing_init(framerate);   
+   if (!renderer_init(scale_factor)) return false;
    input_init();
    scene_init();
 
@@ -74,7 +79,6 @@ bool game_init(void) {
 void game_handle_events(float delta_time) {
    SDL_Event e;
 
-   // TODO: ctrl+c to close?
    while (SDL_PollEvent(&e)) {
       switch (e.type) {
          case SDL_QUIT:
@@ -107,9 +111,38 @@ void game_render(void) {
 }
 
 void game_shutdown(void) {
-   printf("shutting down the game......\n");
+   d_log("shutting down the game......");
    g_game.state = GAME_QUIT;
    input_shutdown();
    renderer_cleanup();
    SDL_Quit();
+}
+
+bool game_handle_flags(int argc, char *argv[], int* logging_mode, float* scale_factor, int* framerate) {
+   for (int i = 1; i < argc; i++) {
+      if (argv[i][0] == '-') {
+         char flag = argv[i][1];
+         
+         if (i + 1 >= argc) {
+            fprintf(stderr, "Missing value for -%c\n", flag);
+            return false;
+         }
+         switch (flag) {
+         case 'l':
+            *logging_mode = atoi(argv[++i]);
+            LOG_VERBOSITY = *logging_mode;
+            break;
+         case 's':
+            *scale_factor = atof(argv[++i]);
+            break;
+         case 'f':
+            *framerate = atoi(argv[++i]);
+            break;
+         default:
+            fprintf(stderr, "Unknown flag: -%c\n", flag);
+            return false;
+         }
+      }
+   }
+   return true;
 }
