43734bcd3a15d9723c8256bbaab7babdb6b0acfb
Teafed <xjakea45x@yahoo.com>
2025-08-29 15:32:36 -0700

    new header for common definitions

---
+++ src/include/def.h
@@ -0,0 +1,15 @@
+#ifndef DEF_H
+#define DEF_H
+
+#include <stdint.h>
+
+typedef uint8_t ui8;
+typedef uint16_t ui16;
+typedef uint32_t ui32;
+typedef uint64_t ui64;
+typedef int8_t si8;
+typedef int16_t si16;
+typedef int32_t si32;
+typedef int64_t si64;
+
+#endif
